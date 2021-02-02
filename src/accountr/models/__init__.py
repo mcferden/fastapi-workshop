724cc4300fb7d2981d4388a7dab4edcf327f537b
Ivan Goncharuk <ivan.goncharuk@antidasoftware.com>
2021-02-02 13:49:37 +0500

    Split models import separate files

---
+++ src/accountr/models/__init__.py
@@ -0,0 +1,6 @@
+from .auth import Token
+from .auth import User
+from .auth import UserCreate
+from .operations import Operation
+from .operations import OperationCreate
+from .operations import OperationUpdate
--- src/accountr/models/operations.py
+++ src/accountr/models/operations.py
@@ -6,27 +6,6 @@ from typing import Optional
 from pydantic import BaseModel
 
 
-class BaseUser(BaseModel):
-    email: str
-    username: str
-
-
-class UserCreate(BaseUser):
-    password: str
-
-
-class User(BaseUser):
-    id: int
-
-    class Config:
-        orm_mode = True
-
-
-class Token(BaseModel):
-    access_token: str
-    token_type: str
-
-
 class OperationKind(str, Enum):
     INCOME = 'income'
     OUTCOME = 'outcome'
