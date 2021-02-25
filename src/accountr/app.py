e262bb3005737431a32d7b5607ce9f82bc77924e
Ivan Goncharuk <ivan.goncharuk@antidasoftware.com>
2021-02-25 12:16:31 +0500

    Document API

--- src/accountr/app.py
+++ src/accountr/app.py
@@ -3,5 +3,26 @@ from fastapi import FastAPI
 from . import api
 
 
-app = FastAPI()
+tags_metadata = [
+    {
+        'name': 'auth',
+        'description': 'Авторизация и регистрация',
+    },
+    {
+        'name': 'operations',
+        'description': 'Создание, редактирование, удаление и просмотр операций',
+    },
+    {
+        'name': 'reports',
+        'description': 'Импорт и экспорт CSV-отчетов',
+    },
+]
+
+app = FastAPI(
+    title='Accountr',
+    description='Сервис учета личных доходов и расходов',
+    version='1.0.0',
+    openapi_tags=tags_metadata,
+)
+
 app.include_router(api.router)
