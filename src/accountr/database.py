c4b1616bcded1f6654825ad09261db7350b53975
Ivan Goncharuk <ivan.goncharuk@antidasoftware.com>
2021-02-25 12:10:33 +0500

    Move database_url to settings, read it from .env

--- src/accountr/database.py
+++ src/accountr/database.py
@@ -1,9 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+from .settings import settings
+
 
 engine = create_engine(
-    'sqlite:///./database.sqlite3',
+    settings.database_url,
     connect_args={'check_same_thread': False},
 )
 
--- src/accountr/settings.py
+++ src/accountr/settings.py
@@ -5,6 +5,8 @@ class Settings(BaseSettings):
     server_host: str = '127.0.0.1'
     server_port: int = 8000
 
+    database_url: str
+
     jwt_secret: str
     jwt_algorithm: str = 'HS256'
     jwt_expires_s: int = 3600
